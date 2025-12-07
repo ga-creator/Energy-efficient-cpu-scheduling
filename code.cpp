b7d88b3efaab784a1bfc9b76f5fd1b30ce34666d
Aprameyya Garg <70478915+ga-creator@users.noreply.github.com>
2025-12-07 18:47:16 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,9 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
+int round_robin(vector<int>at, vector<int> bt){
+    int min=*min_element(at.begin(),at.end());
+    return min;
+}
 int main(){
     int n;
     cout<<"Enter number of processes: ";
     cin>>n;
+    cout<<endl;
     vector<int>at;
     vector<int>bt;
     int count=1;
@@ -15,4 +20,10 @@ int main(){
         bt.push_back(b);
         count++;
     }
+    cout<<endl;
+    int time_quantum;
+    cout<<"Enter time quantum: ";
+    cin>>time_quantum;
+    cout<<endl<<"======Standard Round Robin====="<<endl;
+    cout<<round_robin(at,bt);
 }
