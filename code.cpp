3ae7eb46cb0e09e4bbf30f6d3d7b3dea6937120b
Aprameyya Garg <70478915+ga-creator@users.noreply.github.com>
2025-12-07 18:39:44 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,1 +1,18 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number of processes: ";
+    cin>>n;
+    vector<int>at;
+    vector<int>bt;
+    int count=1;
+    while(n--){
+        int a,b;
+        cout<<"P"<<count<<" ";
+        cin>>a>>b;
+        at.push_back(a);
+        bt.push_back(b);
+        count++;
+    }
+}
