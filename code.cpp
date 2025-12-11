4ee739d381478b46ccc14a28785a4d754f64a165
Aprameyya Garg <70478915+ga-creator@users.noreply.github.com>
2025-12-11 11:25:37 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,43 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 struct Result {
     vector<int> ct, wt, tat, rt;
     int contextSwitches;
     double avgWT, avgTAT, avgRT, energyUsed;
 };
-
 Result round_robin(vector<int> at, vector<int> bt, int n, int tq, bool modified){
-
     vector<int> rem = bt;
     vector<int> ct(n), rt(n, -1), tat(n), wt(n);
     vector<bool> inq(n, false);
-
     queue<int> q;
     int time = *min_element(at.begin(), at.end());
     int totalBurst = accumulate(bt.begin(), bt.end(), 0);
     int contextSwitches = 0;
-
     double energy = 0;
-
     for(int i=0;i<n;i++){
         if(at[i] == time){
             q.push(i);
             inq[i] = true;
         }
     }
-
     while(!q.empty()){
-
         int exec = q.front();
         q.pop();
         inq[exec] = false;
-
         contextSwitches++;
-
         int currentQuantum = tq;
         double power = 8; 
-
         if(modified){
             int qs = q.size() + 1; 
             if(qs <= 2){
@@ -51,9 +40,7 @@ Result round_robin(vector<int> at, vector<int> bt, int n, int tq, bool modified)
                 power = 8;
             }
         }
-
         if(rt[exec] == -1) rt[exec] = time - at[exec];
-
         int run = min(currentQuantum, rem[exec]);
         rem[exec] -= run;
         time += run;
@@ -66,7 +53,6 @@ Result round_robin(vector<int> at, vector<int> bt, int n, int tq, bool modified)
                 inq[i] = true;
             }
         }
-
         if(rem[exec] > 0){
             q.push(exec);
             inq[exec] = true;
@@ -74,13 +60,11 @@ Result round_robin(vector<int> at, vector<int> bt, int n, int tq, bool modified)
             ct[exec] = time;
         }
     }
-
     Result R;
     R.ct = ct;
     R.rt = rt;
     R.contextSwitches = contextSwitches;
     R.energyUsed = energy;
-
     double sumWT = 0, sumTAT = 0, sumRT = 0;
     for(int i=0;i<n;i++){
         tat[i] = ct[i] - at[i];
@@ -95,7 +79,6 @@ Result round_robin(vector<int> at, vector<int> bt, int n, int tq, bool modified)
     R.avgWT = sumWT/n;
     R.avgTAT = sumTAT/n;
     R.avgRT = sumRT/n;
-
     return R;
 }
 void printResult(Result R, vector<int> at, vector<int> bt, string name){
@@ -115,26 +98,19 @@ int main(){
     int n;
     cout<<"Enter number of processes: ";
     cin>>n;
-
     vector<int> at(n), bt(n);
     cout<<"Enter AT and BT for each process:\n";
     for(int i=0;i<n;i++){
         cout<<"P"<<i+1<<": ";
-        cin>>at[i]>>bt[i];
-    }
-
+        cin>>at[i]>>bt[i];}
     int tq;
     cout<<"Enter time quantum for normal RR: ";
     cin>>tq;
-
     Result normalRR = round_robin(at, bt, n, tq, false);
     Result energyRR = round_robin(at, bt, n, tq, true);
-
     printResult(normalRR, at, bt, "Standard Round Robin");
     printResult(energyRR, at, bt, "Energy Efficient Round Robin");
-
     cout << "\n===== Comparison =====\n";
     cout << "Energy saved: " << normalRR.energyUsed - energyRR.energyUsed << endl;
-
     return 0;
 }
